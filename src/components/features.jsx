69b7b7d4ef458b0f1efa4e58ad1184a91661a2a1
aya-m-98 <106470807+aya-m-98@users.noreply.github.com>
2024-02-24 14:11:11 +0200

    massage

---
+++ src/components/features.jsx
@@ -0,0 +1,21 @@
+import { FaFacebook, FaTwitter } from "react-icons/fa";
+import { IoLogoWhatsapp } from "react-icons/io";
+
+export default function Footer() {
+  return (
+    <>
+      <div
+        style={{
+          backgroundColor: "#777",
+          textAlign: "center",
+          height: "80px",
+          padding: "15px",
+        }}
+      >
+        <FaFacebook size={60} style={{ margin: "5px" }} />
+        <FaTwitter size={60} style={{ margin: "5px" }} />
+        <IoLogoWhatsapp size={60} style={{ margin: "5px" }} />
+      </div>
+    </>
+  );
+}
